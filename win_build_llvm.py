e4fe2353d8914e21b934369be795fb24f045f1f6
Marek Surovič <xsurov03@stud.fit.vutbr.cz>
2015-10-11 03:44:57 +0200

    Reworked the input sample compilation scripts and added obfuscation. Also added generation of mutations. This way the sample generation part should be done.
    LLVM build script for windows was also rewritten into python 2.7.x

---
+++ win_build_llvm.py
@@ -0,0 +1,26 @@
+import os
+import sys
+import shutil
+import subprocess
+
+def main():
+    ConfGen = "Visual Studio 14 2015 Win64"
+    ConfFlags = "-DLLVM_TARGETS_TO_BUILD:STRING=X86"
+    ConfSrcDir = "..\llvm-3.7.0"
+
+    BuildDir = "./tools/"
+    BuildType = "Release"
+    BuildJobs = "/maxcpucount:2"
+
+    if len(sys.argv) == 2 and sys.argv[1] == "clean":
+        shutil.rmtree(BuildDir)
+
+    if not os.path.exists(BuildDir):
+        os.mkdir(BuildDir)
+    
+    os.chdir(BuildDir)
+    subprocess.call(["cmake", "-G", ConfGen, ConfFlags, ConfSrcDir])
+    subprocess.call(["cmake", "--build", '.', "--config", BuildType, "--", BuildJobs])
+    
+if __name__ == '__main__':
+    main()
